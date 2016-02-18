67f5a3057b2a3ca68d0ecde0aff0335e11910a07
Matt Stjernholm <thew88@gmail.com>
2016-02-18 12:25:52 -0700

    add collision tests

--- test/collision-detection-test.js
+++ test/collision-detection-test.js
@@ -7,44 +7,57 @@ const GatePainter = require('../lib/gate-painter')
 const CollisionDetect = require('../lib/collision-detect')
 const Ball = require('../lib/ball')
 const ViewPort = require('../lib/viewport');
-
+const BallEdges = require('../lib/helpers/ball-edges')
+const PowerupEdges = require('../lib/helpers/powerup-edges')
+const GatesEdges = require('../lib/helpers/gates-edges')
+const GateExpander = require('../lib/helpers/gate-expander-helper')
+const Game = require('../lib/game')
 
 describe('Collisions', function() {
 
   context('detection', function() {
 
-    it('a ball that has an x position less than a gateStart position and a y position equal to the top of a gate, it will move up with the gate', function() {
+    var canvas = document.getElementById("game");
+    var ctx = canvas.getContext('2d');
+    var gates = [new Gate({}, canvas)]
+    var ball = new Ball({}, canvas)
+    var game = new Game(canvas, ctx, {speed: 15, score: 40});
+    var expandGates = ""
+    var powerup = ""
+    GateBuilder(gates, canvas)
 
-      var canvas = new ViewPort(300, 200)
-      var gates = [new Gate({}, canvas)]
-      var ball = new Ball({r: 10}, canvas)
-      var score = []
-      GateBuilder(gates, canvas)
+    var ballSpeed = 5;
+    var acceleration = 0.5;
+    var fallSpeed = 5;
 
-      var gate = gates[0]
-      gate.y = 75
-      gate.gateHeight = 15
-      gate.gateStart = 150
-      var centerOfGate = gate.y
-      var topOfGate = centerOfGate + gate.gateHeight
+    var gate = gates[0]
+    gate.y = 88
+    gate.gateHeight = 15
+    gate.gateStart = 150
+    gate.gateEnd = 190
+    var centerOfGate = gate.y
+    var topOfGate = centerOfGate + gate.gateHeight
 
-      ball.y = 80
-      ball.x = 50
-      var centerOfBall = ball.y
-      var bottomOfBall = centerOfBall + ball.r
+    ball.y = 80
+    ball.x = 50
 
-      assert.equal(topOfGate, bottomOfBall);
-      assert(ball.x < gate.gateStart);
-
-      CollisionDetect(ball, gates, score)
-
-      gate.update(10, canvas)
-
-      assert.equal(gate.y, 65)
-      assert.equal(bottomOfBall, 65)
-      // how do we test this?
-
+    it('ball y is correct when ball collides with left gate', function() {
+      assert.equal(BallEdges.bottomOfBall(ball), gate.y)
     });
+
+    it('ball x is correct when ball collides with left gate', function() {
+      assert(BallEdges.rightOfBall(ball) < gate.gateStart)
+    });
+
+    it('ball y is correct when ball collides with right gate', function() {
+      assert.equal(BallEdges.bottomOfBall(ball), gate.y)
+    });
+
+    it('ball x is correct when ball collides with right gate', function() {
+      ball.x = 200
+      assert(BallEdges.leftOfBall(ball) > gate.gateEnd)
+    });
+
   });
 
 });
